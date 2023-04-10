7b12e8d3c41cac08a3d45a305e7ba90f4fd6f795
UnelDev <enee.prog@gmail.com>
2023-04-10 21:39:45 +0200

    add test in testPassword

--- test/testPassword.test.ts
+++ test/testPassword.test.ts
@@ -76,4 +76,16 @@ describe('GET /testPassword', () => {
 		expect(res.body).toHaveProperty('status', 400);
 		expect(res.body).toHaveProperty('message', 'the testPassword must be sha512');
 	});
+
+	it('should return a 404 if email is not link at user', async () => {
+		const res = await req
+			.get('/api/testPassword')
+			.send({
+				email: 'example@example.com',
+				testPassword: 'EE26B0DD4AF7E749AA1A8EE3C10AE9923F618980772E473F8819A5D4940E0DB27AC185F8A0E1D5F84F88BC887FD67B143732C304CC5FA9AD8E6F57F50028A8FF'
+			});
+		expect(res.status).toEqual(404);
+		expect(res.body).toHaveProperty('status', 404);
+		expect(res.body).toHaveProperty('message', 'user not fond');
+	});
 });
