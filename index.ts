05e321d6c358bf422d23e8e419e5e4bc954da3f8
Wiwok <Emil.dechenaud@gmail.com>
2023-04-27 22:47:30 +0200

    Create changeAdminPassword

--- index.ts
+++ index.ts
@@ -4,34 +4,22 @@ import cors from 'cors';
 import router from './routes';
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
-let started = false;
 
-export default function startServer() {
-	if (started) {
-		console.log('server already started');
-		return;
-	}
-	started = true;
-	// Connect to MongoDB using Mongoose
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.URI).then(() => {
+	console.log('Successfully connected to MongoDB');
+}).catch((error) => {
+	console.log('Error connecting to MongoDB:', error);
+});
 
-	mongoose.connect(process.env.URI)
-		.then(() => {
-			console.log('Successfully connected to MongoDB');
-		}).catch((error) => {
-			console.log('Error connecting to MongoDB:', error);
-		});
+// Create an instance of the Express app
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api', router);
 
-	// Create an instance of the Express app
-	const app = express();
-	app.use(express.json());
-	app.use(cors());
-	app.use('/api', router);
-
-	// Start the Express app
-	const port = 8082;
-	app.listen(port, () => {
-		console.log(`listening at http://localhost:${port}`);
-	});
-}
-
-startServer();
+// Start the Express app
+const port = 8082;
+app.listen(port, () => {
+	console.log(`Listening at http://localhost:${port}`);
+});
